13c6cb99682c145e21d76307d8de92219faaf468
ServerToMbak <server78135@gmail.com>
2023-05-11 20:07:41 +0300

    update

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[Controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        
+        public PlatformsController()
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult TextInBoundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # CommandService");
+
+            return Ok("Inbound test of from platforms Controller");
+        }
+
+    }
+}
